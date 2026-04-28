da94e9518fa728a241578fc82a33d19dfc793999
Pranav Salunkhe <pranav.salunkhe38@gmail.com>
2026-04-28 23:10:03 +0530

    "28 April 2026 0000 Minimizing Coin"

---
+++ solution/0000_Minimizing_Coin.cpp
@@ -0,0 +1,195 @@
+// LC: 0000 Minimizing Coin
+// Title: Minimizing Coin
+// Difficulty: medium
+// Date: 28 April 2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- types ----------
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// ---------- macros ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (a); i >= (b); --i)
+
+// ---------- debug (compile with -DLOCAL) ----------
+#ifdef LOCAL
+template <typename T>
+void _print(const T &x) { cerr << x; }
+template <typename A, typename B>
+void _print(const pair<A, B> &p)
+{
+    cerr << "(";
+    _print(p.first);
+    cerr << ", ";
+    _print(p.second);
+    cerr << ")";
+}
+template <typename T>
+void _print(const vector<T> &v)
+{
+    cerr << "[";
+    for (int i = 0; i < (int)v.size(); ++i)
+    {
+        if (i)
+            cerr << ", ";
+        _print(v[i]);
+    }
+    cerr << "]";
+}
+template <typename T>
+void _print(const set<T> &s)
+{
+    cerr << "{";
+    bool f = 0;
+    for (auto &x : s)
+    {
+        if (f)
+            cerr << ", ";
+        f = 1;
+        _print(x);
+    }
+    cerr << "}";
+}
+template <typename K, typename V>
+void _print(const map<K, V> &m)
+{
+    cerr << "{";
+    bool f = 0;
+    for (auto &kv : m)
+    {
+        if (f)
+            cerr << ", ";
+        f = 1;
+        _print(kv);
+    }
+    cerr << "}";
+}
+
+#define dbg(x) cerr << #x << " = ", _print(x), cerr << "\n"
+#else
+#define dbg(x) ((void)0)
+#endif
+
+// ---------- constants ----------
+const ll INFLL = (ll)4e18;
+const int INF = (int)2e9;
+
+// ---------- math helpers ----------
+ll gcdll(ll a, ll b) { return std::gcd(a, b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+
+ll modpow(ll a, ll e, ll mod)
+{
+    ll r = 1 % mod;
+    a %= mod;
+    while (e > 0)
+    {
+        if (e & 1)
+            r = (__int128)r * a % mod;
+        a = (__int128)a * a % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
+// ---------- IO helpers ----------
+template <typename T>
+void read_vec(vector<T> &v)
+{
+    for (auto &x : v)
+        cin >> x;
+}
+
+template <typename T>
+void print_vec(const vector<T> &v, char sep = ' ')
+{
+    for (int i = 0; i < (int)v.size(); ++i)
+    {
+        if (i)
+            cout << sep;
+        cout << v[i];
+    }
+    cout << "\n";
+}
+
+// ---------- solve ----------
+
+int rec(vector<int>& v, int i, int x,vector<vector<int>>& dp)
+{
+        if(x == 0) return 0;
+
+    if(i == v.size()) return INT_MAX;
+
+    if(dp[i][x] != -1) return dp[i][x];
+
+    int val = rec(v, i+1, x,dp);
+    
+    if(x - v[i] >= 0){
+        int y = rec(v,i, x - v[i],dp);
+        if(y != INT_MAX)
+            val = min(val, 1+ y);
+    }
+
+    return dp[i][x] = val;
+
+
+}
+void solve()
+{
+    int n,x;
+    cin>>n>>x;
+    vector<int> v(n);
+    read_vec(v);
+
+    vector<int> dp(x+1,INT_MAX);
+
+    dp[0] = 0;
+
+for(int sum = 1; sum <= x ; sum++){
+    for(int i = 0 ; i < n; i++){
+       if (sum - v[i] >= 0 && dp[sum - v[i]] != INT_MAX) {
+    dp[sum] = min(dp[sum], dp[sum - v[i]] + 1);
+}
+}
+}
+
+    int ans = dp[x];
+
+    if (ans == INT_MAX) cout << -1 << endl;
+    else cout << ans << endl;
+
+
+}
+
+// ---------- main ----------
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+
+    while (T--)
+        solve();
+
+    return 0;
+}
